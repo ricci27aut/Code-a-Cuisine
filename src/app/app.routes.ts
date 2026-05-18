d5d78027811ff22e7943be982160e0ae9b6698e0
Riccardo Schöpf <rischoepf38@gmail.com>
2026-05-18 18:37:21 +0200

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { StartScreen } from '../app/components/start-screen/start-screen'
+
+export const routes: Routes = [
+    {
+        path: "",
+        component: StartScreen
+    },
+];
---
+++ src/app/components/start-screen/start-screen.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-screen',
+  imports: [],
+  templateUrl: './start-screen.html',
+  styleUrl: './start-screen.scss',
+})
+export class StartScreen {}
