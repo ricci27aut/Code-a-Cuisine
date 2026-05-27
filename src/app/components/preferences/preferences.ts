b512197d345c23137d521485f17565b1e51cef80
Riccardo Schöpf <rischoepf38@gmail.com>
2026-05-27 18:20:49 +0200

    add basic structor for preferences

--- src/app/components/preferences/preferences.ts
+++ src/app/components/preferences/preferences.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-preferences',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './preferences.html',
   styleUrl: './preferences.scss',
 })
-export class Preferences {}
+export class Preferences {
+  countries = ['German', 'Italie', 'India', 'Japanese', 'Fusion'];
+  preferencs = ['Vegetarien', 'Vegan', 'Keto', 'No preferences']
+  persons: number = 1;
+  portions: number = 1;
+
+  countUp(list: 'persons' | 'portions'): void {
+    if (this[list] >= 8) {
+      return;
+    }
+    this[list] += 1;
+  }
+
+  countDown(list: 'persons' | 'portions'): void {
+    if (this[list] == 1) {
+      return
+    }
+    this[list] -= 1;
+  }
+}
